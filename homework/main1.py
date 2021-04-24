1cb5fb385b2862eb3b5047321934ee344ee3c23f
zagjle <82961658+zagjle@users.noreply.github.com>
2021-04-24 15:50:54 +0300

    Add files via upload

---
+++ homework/main1.py
@@ -0,0 +1,102 @@
+# Переменная - это ячейка в памяти. Переменные можем запрашивать от пользователя
+# или сами устанавливать. Переменные позволяют хранить информацию и использовать ее в дальнейшем.
+
+# Типы переменных в Python не обьявляются очевидно, но они присутствуют.
+# name_1 = 'Hello World!'
+# name_2 = 100
+# word = "м'яч"
+#
+# print(word)
+#
+# name = input("Введите Ваше имя: ")
+# surname = input("Please, enter your surname: ")
+# age = input("Please, enter your age: ")
+# print("Ваше имя и фамилия: ", name, surname)
+# print("Возраст: ", age)
+#
+# city = input("укажите свой город: ")
+# print("city: ", city)
+
+
+#
+# print("Enter your city: ")
+# your_locate=input()
+# print("Your city is ", your_locate)
+#
+# name = input("Please, enter your name: ")
+# surname = input("Please, enter your surname: ")
+# print("Name: {1}, surname: {0}.". format(name, surname))
+
+
+# surname = input("Укажите вашу фамилию: ")
+# name = input("Введите ваше имя: ")
+# otchestvo = input("Укажите ваше отчество: ")
+# telephon = input("Укажите свой номер телефона: ")
+# age = input("Укажите ваш возраст: ")
+# city = input("Укажите свой город: ")
+# music = input("Укажите ваш любимый жанр музыки: ")
+# street = input("Укажите улицу на которой вы живёте: ")
+# print("Ваше имя и фамилия и отчество: ", name, surname, otchestvo)
+# print("Ваш возраст: ", age)
+# print("Ваша улица: ", street)
+# print("Ваш город: ", city)
+# print("Ваш номер телефон: ", telephon)
+# print("Ваш любимый жанр: ", music)
+
+#or
+
+# name = input("Введите ваше имя: ")
+# surname = input("Введите вашу фамилию: ")
+# otchestvo = input("Укажите ваше отчество: ")
+# print("Name: {0}, surname: {1}, patronymic: {2}.". format(name, surname, patronymic))
+
+
+
+
+
+
+# num1 = int(input("Введите число: "))
+# num2 = int(input("Введите число: "))
+# res = num1 + num2
+# print("Результат: ", res)
+
+
+# num1 = 5
+# # num1 = num1 + 8
+# num1 += 8
+# print(num1)
+
+# print("Введите 1-е число: ")
+# a=int(input())
+# print("Введите 2-е число: ")
+# b=int(input())
+
+
+# a=14
+# b=5
+# c = a //b
+# d = a//b
+# g = a %b
+#
+# print(c)
+# print(d)
+# print(g)
+
+# s=2
+# q=4
+# w1 = s * q #обычное умножение
+# w2 = s**q #возведение в степень
+# print("обычное умножение: {0} * {1} = {2}".format(s, q, w1))
+# print("возведение в степень: {0} в степени {1} = {2}".format(s, q, w2))
+
+
+
+# num3 = abs(-15)
+# print(num)
+# z=1+2j
+# print(z, "тип переменной: ", type (z))
+
+z1 = int(input("Введите число: "))
+print(z1)
+print(z1, "тип переменной: ", type (z1))
+
